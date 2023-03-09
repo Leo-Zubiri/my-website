054331600dd564c3e72ea59c9f73f6dc50eaf4f8
Leo-Zubiri <leo.zubiri@outlook.com>
2023-03-08 22:20:33 -0600

    Add text animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,15 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
+import './index.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+    <header className="heroBanner">
+      <div className="titleContainer">
+        <h1 className="slide-in-elliptic-top-fwd">{siteConfig.title}</h1>
+        <h2 className="expand"> {siteConfig.tagline} </h2>
       </div>
     </header>
   );
