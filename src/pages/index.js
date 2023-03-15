41508f2f574caeb8364367c78396cb40cf7ba0b0
Leo-Zubiri <leo.zubiri@outlook.com>
2023-03-15 15:59:02 -0600

    add bg and glass effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,15 +12,15 @@ import './index.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className="heroBanner">
-      <div className="titleContainer">
+    <header className="heroBanner myBanner">
+      <div className="titleContainer glass">
         <h1 className="slide-in-elliptic-top-fwd title-devsite">{`< ${siteConfig.title} />`}</h1>
 
         <img src={useBaseUrl("/img/logo2.png")} alt="My perfil photo"  
           className='scale-up-center main_perfil_photo'
         />
 
-        <h2 className="fade-in italic-phrase"> {siteConfig.tagline} </h2>
+        <h2 className="fade_in italic-phrase"> {siteConfig.tagline} </h2>
       </div>
     </header>
   );
