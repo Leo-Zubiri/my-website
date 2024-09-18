53aeeaf1d7121e0ec151584d3f9eb165a81eb082
Leo Zubiri <Leo.Zubiri@outlook.com>
2024-09-17 22:59:08 -0600

    Add [My notes] new entry dot NET

---
+++ docs/NET/index.md
@@ -0,0 +1,11 @@
+# .NET
+
+![](../../static/img/card-logo/dotnet-logo.png)
+
+.NET es una plataforma de código abierto para crear aplicaciones de escritorio, web y móviles que se pueden ejecutar de forma nativa en cualquier sistema operativo. El sistema .NET incluye herramientas, bibliotecas y lenguajes que admiten el desarrollo de software moderno, escalable y de alto rendimiento. Una comunidad de desarrolladores activa mantiene y apoya la plataforma .NET.
+
+
+- **.NET Framework** .NET Framework es la implementación de .NET original. Admite la ejecución de sitios web, servicios, aplicaciones de escritorio y más en Windows. Microsoft lanzó .NET Framework a principios de la década de 1990.
+- **.NET Core**. Microsoft lanzó .NET Core a finales de 2014 para permitir el soporte multiplataforma para los desarrolladores de .NET. La compañía renombro .NET Core a solo **.NET**.
+
+.NET es de código abierto, lo que significa que cualquiera puede tener acceso para usarlo, leerlo y modificarlo libremente. Una comunidad activa de desarrolladores mantiene y mejora el software de .NET. .NET Foundation es una organización independiente sin fines de lucro establecida para ayudar a la comunidad .NET. Proporciona recursos de aprendizaje, proyectos .NET de código abierto y varios eventos para los desarrolladores de .NET.
--- docs/content.config.js
+++ docs/content.config.js
@@ -1,5 +1,40 @@
 export const topics = [
     {
+        name: 'Git',
+        img: '/img/card-logo/git-logo.png',
+        path: '/docs/git'
+    },
+    {
+        name: 'NET',
+        img: '/img/card-logo/dotnet-logo.png',
+        path: '/docs/NET'
+    },
+    {
+        name: 'Python',
+        img: '/img/card-logo/python-logo.png',
+        path: '/docs/python'
+    },
+    {
+        name: 'PHP',
+        img: '/img/php.jpg',
+        path: '/docs/php'
+    },
+    {
+        name: 'Laravel',
+        img: '/img/Laravel.jpg',
+        path: '/docs/Laravel'
+    },
+    {
+        name: 'PostgreSQL',
+        img: '/img/card-logo/postgres-logo.png',
+        path: '/docs/postgres'
+    },
+    {
+        name: 'SQL Server',
+        img: '/img/sqlserver.png',
+        path: '/docs/sqlserver'
+    },
+    {
         name: 'CSS',
         img: '/img/css.png',
         path: '/docs/css'
@@ -20,43 +55,13 @@ export const topics = [
         path: '/docs/nodejs'
     },
     {
-        name: 'PHP',
-        img: '/img/php.jpg',
-        path: '/docs/php'
-    },
-    {
-        name: 'Laravel',
-        img: '/img/Laravel.jpg',
-        path: '/docs/Laravel'
-    },
-    {
         name: 'Angular',
         img: '/img/angular.png',
         path: '/docs/Angular'
-    },
-    {
-        name: 'SQL Server',
-        img: '/img/sqlserver.png',
-        path: '/docs/sqlserver'
     },
     {
         name: 'Astro',
         img: '/img/card-logo/astro-logo.png',
         path: '/docs/Astro'
     },
-    {
-        name: 'Git',
-        img: '/img/card-logo/git-logo.png',
-        path: '/docs/git'
-    },
-    {
-        name: 'PostgreSQL',
-        img: '/img/card-logo/postgres-logo.png',
-        path: '/docs/postgres'
-    },
-    {
-        name: 'Python',
-        img: '/img/card-logo/python-logo.png',
-        path: '/docs/python'
-    }
 ];
